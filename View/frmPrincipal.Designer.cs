a9b23a5536571dbb47ca0323a4c6e8510f87983e
Pepito <juan.agustin.gonzalez99@gmail.com>
2019-05-02 19:32:08 -0300

    primer commit

---
+++ View/frmPrincipal.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace View
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnsMenu = new System.Windows.Forms.MenuStrip();
+            this.tsmAlumno = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmProfesor = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnsMenu
+            // 
+            this.mnsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmAlumno,
+            this.tsmProfesor});
+            this.mnsMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnsMenu.Name = "mnsMenu";
+            this.mnsMenu.Size = new System.Drawing.Size(1085, 24);
+            this.mnsMenu.TabIndex = 3;
+            this.mnsMenu.Text = "Menu";
+            // 
+            // tsmAlumno
+            // 
+            this.tsmAlumno.Name = "tsmAlumno";
+            this.tsmAlumno.Size = new System.Drawing.Size(62, 20);
+            this.tsmAlumno.Text = "Alumno";
+            this.tsmAlumno.Click += new System.EventHandler(this.tsmAlumno_Click);
+            // 
+            // tsmProfesor
+            // 
+            this.tsmProfesor.Name = "tsmProfesor";
+            this.tsmProfesor.Size = new System.Drawing.Size(63, 20);
+            this.tsmProfesor.Text = "Profesor";
+            this.tsmProfesor.Click += new System.EventHandler(this.tsmProfesor_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 637);
+            this.Controls.Add(this.mnsMenu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnsMenu;
+            this.Name = "frmPrincipal";
+            this.Text = "Aplicación";
+            this.mnsMenu.ResumeLayout(false);
+            this.mnsMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsMenu;
+        private System.Windows.Forms.ToolStripMenuItem tsmAlumno;
+        private System.Windows.Forms.ToolStripMenuItem tsmProfesor;
+    }
+}
---
+++ View/frmPrincipal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProfesor_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void tsmAlumno_Click(object sender, EventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(frmAlumno))
+                {
+                    form.Activate();
+                    return;
+                }
+            }
+
+            frmAlumno frmAlumno = new frmAlumno
+            {
+                MdiParent = this,
+                WindowState = FormWindowState.Maximized,
+                ControlBox = false
+            };
+            frmAlumno.Show();
+        }
+
+        private void tsmProfesor_Click(object sender, EventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.GetType() == typeof(frmProfesor))
+                {
+                    form.Activate();
+                    return;
+                }
+            }
+
+            frmProfesor frmProfesor = new frmProfesor
+            {
+                MdiParent = this,
+                WindowState = FormWindowState.Maximized,
+                ControlBox = false
+            };
+            frmProfesor.Show();
+        }
+    }
+}
